865c6502ded2eef15033016864ac2ef11b717afa
Philipp Berthold <philipp.berthold@gmail.com>
2021-07-28 01:07:31 +0200

    Android Theme

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -6,6 +6,7 @@
 #ifdef ANDROID
 #include <QtAndroid>
 #include <QtAndroidExtras>
+#include <QStyleFactory>
 #endif
 
 int main(int argc, char *argv[])
@@ -19,5 +20,28 @@ int main(int argc, char *argv[])
 
     MainWindow w(&core);
     w.show();
+
+    #ifdef ANDROID
+        qApp->setStyle(QStyleFactory::create("fusion"));
+        QPalette palette;
+        palette.setColor(QPalette::Window, QColor(53,53,53));
+        palette.setColor(QPalette::WindowText, Qt::white);
+        palette.setColor(QPalette::Base, QColor(15,15,15));
+        palette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+        palette.setColor(QPalette::ToolTipBase, Qt::white);
+        palette.setColor(QPalette::ToolTipText, Qt::white);
+        palette.setColor(QPalette::Text, Qt::white);
+        palette.setColor(QPalette::Button, QColor(53,53,53));
+        palette.setColor(QPalette::ButtonText, Qt::white);
+        palette.setColor(QPalette::BrightText, Qt::red);
+
+        palette.setColor(QPalette::Highlight, QColor(142,45,197).lighter());
+        palette.setColor(QPalette::HighlightedText, Qt::black);
+
+        palette.setColor(QPalette::Disabled, QPalette::Text, Qt::darkGray);
+        palette.setColor(QPalette::Disabled, QPalette::ButtonText, Qt::darkGray);
+        qApp->setPalette(palette);
+    #endif
+
     return a.exec();
 }
